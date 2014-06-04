50ccca970bb4d2e0f98571cd4c6d97fa8102430c
Liu DongMiao <liudongmiao@gmail.com>
2014-06-04 16:40:23 +0800

    remove unused codes

--- src/biz/bokhorst/xprivacy/Activity.java
+++ src/biz/bokhorst/xprivacy/Activity.java
@@ -3,22 +3,9 @@ package biz.bokhorst.xprivacy;
 import me.piebridge.util.GingerBreadUtil;
 
 import android.os.Build;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 public class Activity extends ActionBarActivity {
-
-	private static String BUNDLE = "activity_compat_bundle";
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD_MR1 && getIntent().hasExtra(BUNDLE)
-				&& savedInstanceState == null) {
-			savedInstanceState = getIntent().getExtras().getBundle(BUNDLE);
-		}
-		super.onCreate(savedInstanceState);
-	}
-
 	@Override
 	public void recreate() {
 		GingerBreadUtil.recreate(this);
